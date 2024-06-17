75609cdbf1896b865b995c44e8f41804b2273b2a
DenisVoron <Denis.voronko@gmail.com>
2024-06-17 20:32:09 +0300

    work over btn

--- app/components/ProjectsBtn.tsx
+++ app/components/ProjectsBtn.tsx
@@ -7,14 +7,16 @@ const ProjectsBtn: FC = (): JSX.Element => {
   return (
     <div className="mx-auto xl:mx-0">
       <Link
-        href={"/work"}
-        className="relative w-[165px] h-[165px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+        href="/work"
+        rel="preload prefetch"
+        className="relative z-20 w-[165px] h-[165px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
       >
         <Image
           src={"/rounded-text.png"}
           width={121}
           height={128}
           alt=""
+          priority={true}
           className="animate-spin-slow w-auto h-full max-w-[121px] max-h-[128px]"
         />
         <HiArrowRight className="absolute text-3xl group-hover:translate-x-2 transition-all duration-300" />
